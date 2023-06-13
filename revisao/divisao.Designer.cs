a17671ce6307b6ebc8db4afd1371760ac07f41fc
alessandra Marques <alessandra2309@gmail.com>
2023-06-13 13:30:30 -0400

    inicio da revisão

---
+++ revisao/divisao.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace revisao
+{
+    partial class divisao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_valor1 = new System.Windows.Forms.TextBox();
+            this.tx_valor2 = new System.Windows.Forms.MaskedTextBox();
+            this.bt_calcular = new System.Windows.Forms.Button();
+            this.lb_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tx_valor1
+            // 
+            this.tx_valor1.Location = new System.Drawing.Point(79, 84);
+            this.tx_valor1.Name = "tx_valor1";
+            this.tx_valor1.Size = new System.Drawing.Size(174, 20);
+            this.tx_valor1.TabIndex = 0;
+            this.tx_valor1.TextChanged += new System.EventHandler(this.tx_valor1_TextChanged);
+            // 
+            // tx_valor2
+            // 
+            this.tx_valor2.Location = new System.Drawing.Point(79, 150);
+            this.tx_valor2.Name = "tx_valor2";
+            this.tx_valor2.Size = new System.Drawing.Size(174, 20);
+            this.tx_valor2.TabIndex = 1;
+            // 
+            // bt_calcular
+            // 
+            this.bt_calcular.Location = new System.Drawing.Point(124, 239);
+            this.bt_calcular.Name = "bt_calcular";
+            this.bt_calcular.Size = new System.Drawing.Size(75, 34);
+            this.bt_calcular.TabIndex = 2;
+            this.bt_calcular.Text = "calcular";
+            this.bt_calcular.UseVisualStyleBackColor = true;
+            this.bt_calcular.Click += new System.EventHandler(this.bt_calcular_Click);
+            // 
+            // lb_resultado
+            // 
+            this.lb_resultado.AutoSize = true;
+            this.lb_resultado.Location = new System.Drawing.Point(44, 214);
+            this.lb_resultado.Name = "lb_resultado";
+            this.lb_resultado.Size = new System.Drawing.Size(35, 13);
+            this.lb_resultado.TabIndex = 3;
+            this.lb_resultado.Text = "label1";
+            this.lb_resultado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // divisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 337);
+            this.Controls.Add(this.lb_resultado);
+            this.Controls.Add(this.bt_calcular);
+            this.Controls.Add(this.tx_valor2);
+            this.Controls.Add(this.tx_valor1);
+            this.Name = "divisao";
+            this.Text = "divisao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_valor1;
+        private System.Windows.Forms.MaskedTextBox tx_valor2;
+        private System.Windows.Forms.Button bt_calcular;
+        private System.Windows.Forms.Label lb_resultado;
+    }
+}
---
+++ revisao/divisao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace revisao
+{
+    public partial class divisao : Form
+    {
+        public divisao()
+        {
+            InitializeComponent();
+            lb_resultado.Text = "";
+        }
+
+        private void tx_valor1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_calcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = Convert.ToInt32(tx_valor1.Text);
+                int b = Convert.ToInt32(tx_valor2.Text);
+
+                int c = a / b;
+                lb_resultado.Text = c.ToString();
+                MessageBox.Show(c.ToString());
+            }catch(Exception ex)
+            {
+                MessageBox.Show("erro ao dividir!");
+            }
+            finally
+            {
+                MessageBox.Show("vai executar de qualquer forma!");
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
